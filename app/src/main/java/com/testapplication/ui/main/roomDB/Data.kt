b7d8ba0b1b4a1f01800d41ef1c687b5e6ba372d3
vikas_solanki <vikas.solanki@biocube.ai>
2022-12-07 10:25:16 +0530

    initial

--- app/src/main/java/com/testapplication/ui/main/roomDB/Data.kt
+++ app/src/main/java/com/testapplication/ui/main/roomDB/Data.kt
@@ -6,7 +6,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
-
+//DATA Class
 @Parcelize
 @Entity(tableName = "User Info")
  data class Data(
